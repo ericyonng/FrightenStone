b7dd91d8f193e14153b0fc0173f3cbe4f7ae97f3
songhuiya <120453674@qq.com>
2019-07-30 23:08:57 +0800

    【base】test对象池

--- TestSuit/TestSuit/TestInst/TestObjPool.h
+++ TestSuit/TestSuit/TestInst/TestObjPool.h
@@ -8,7 +8,7 @@
 #define __FS_THREAD_SAFE__ 0
 
 #undef TEST_OBJ_NUM
-#define TEST_OBJ_NUM    1000000
+#define TEST_OBJ_NUM    1000
 
 class TestObjPoolObj
 {
@@ -17,14 +17,14 @@ public:
     TestObjPoolObj() {}
     ~TestObjPoolObj() 
     {
-        std::cout << "Îö¹¹" << std::endl;
+        //std::cout << "Îö¹¹" << std::endl;
     }
 
 private:
     char _char;
 };
 
-OBJ_POOL_CREATE_IMPL(TestObjPoolObj, _objPoolHelper, TEST_OBJ_NUM)
+OBJ_POOL_CREATE_IMPL(TestObjPoolObj, _objPoolHelper, 5)
 
 class TestObjPoolObj2
 {
@@ -52,11 +52,17 @@ public:
         std::cout << "escape :" << (timeNow2 - timeNow1).GetTotalMicroSeconds() << std::endl;
 
         timeNow1.FlushTime();
+        TestObjPoolObj *ptr[TEST_OBJ_NUM] = {};
         for(Int32 i = 0; i < TEST_OBJ_NUM; ++i)
         {            
-            new TestObjPoolObj;
+            ptr[i] = new TestObjPoolObj;
         }
         timeNow2.FlushTime();
+        for(Int32 i = 0; i < TEST_OBJ_NUM; ++i)
+        {
+            delete  ptr[i];
+        }
+
         std::cout << "escape :" << (timeNow2 - timeNow1).GetTotalMicroSeconds() << std::endl;
         std::cout << "memleak:" << TestObjPoolObj::GetMemleakNum() << std::endl;
 //         g_Log->w<TestObjPool>(_LOGFMT_("HELLO"));
