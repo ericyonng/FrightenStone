f8a0d3e48502813b0d4ca447ab633bad5d6cbc23
ericyonng <120453674@qq.com>
2019-08-23 08:52:59 +0800

    【base】缓冲数组

---
+++ base/common/net/Defs/FS_NetBufferArrayImpl.h
@@ -0,0 +1,40 @@
+#ifdef __Base_Common_Net_Defs_FS_NetBufferArray_H__
+#pragma once
+
+FS_NAMESPACE_BEGIN
+
+inline FS_NetBufferArray::FS_NetBufferArray(Int32 sizeBuffer)
+    :_bufferSize(sizeBuffer)
+{
+    _buffers.push_back(new FS_NetBuffer(_bufferSize));
+}
+
+inline FS_NetBufferArray::~FS_NetBufferArray()
+{
+    STLUtil::DelListContainer<decltype(_buffers), AssistObjsDefs::SelfRelease>(_buffers);
+}
+
+inline char *FS_NetBufferArray::GetData()
+{
+    return _buffers.front()->GetData();
+}
+
+inline bool FS_NetBufferArray::Push(const char *data, Int32 len)
+{
+    if(!_buffers.back()->Push(data, len))
+    {
+        auto newBuffer = new FS_NetBuffer(_bufferSize);
+        _buffers.push_back(newBuffer);
+        return newBuffer->Push(data, len);
+    }
+
+    return true;
+}
+
+inline void Pop(Int32 len)
+{
+}
+
+FS_NAMESPACE_END
+
+#endif
