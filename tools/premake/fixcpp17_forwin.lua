a4e43436e5c7828fe47007e6a9b6eaa1bd03d570
songhuiya <120453674@qq.com>
2019-11-25 21:17:07 +0800

    【base】premake构建后支持vs的c++17语言特性

---
+++ tools/premake/fixcpp17_forwin.lua
@@ -0,0 +1,46 @@
+--  @file   premake5.lua
+-- #########################################################################
+-- Global compile settings
+
+-- python tool define
+IS_WINDOWS = string.match(_ACTION, 'vs') ~= nil
+local PY = IS_WINDOWS and "$(ProjectDir)../../tools/py.exe" or "python"
+
+-- header directory
+FS_HEADER_DIR = "../../base/base/"
+
+-- All libraries output directory
+FS_OUTPUT_BASE_DIR = "../../output/" .. _ACTION
+
+-- root directory
+FS_ROOT_DIR = "../../"
+
+FS_BASE_PROJ_NAME = "fsbase"
+
+FS_BUILD_DIR = "../../build/"
+
+if IS_WINDOWS then
+    FS_OUTPUT_DIR = FS_OUTPUT_BASE_DIR .. "/$(Configuration)"
+else
+    FS_OUTPUT_DIR = FS_OUTPUT_BASE_DIR .. "/$(config)"
+end
+-----------------------------------------------------------------------------------------------------------
+	
+if IS_WINDOWS then
+
+-- 开启c++17
+local solutionPath = FS_BUILD_DIR .. _ACTION .. "/" .. FS_BASE_PROJ_NAME .. ".vcxproj";
+print(solutionPath)
+local solutionFile = io.open(solutionPath, "r")
+print("1")
+local str = solutionFile:read("a")
+print("2")
+print(str)
+local finalProj = string.gsub(str, "stdcpp14", "stdcpp17")
+print(finalProj)
+solutionFile:close()
+solutionFile = io.open(solutionPath, "w")
+solutionFile:write(finalProj)
+solutionFile:close()
+end
+
