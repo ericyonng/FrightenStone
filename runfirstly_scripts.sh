3005973a745ec98f41a9ec8b5feb69669f4fbf32
songhuiya <120453674@qq.com>
2019-11-30 16:06:25 +0800

    【frame】脚本

--- runfirstly_scripts.sh
+++ runfirstly_scripts.sh
@@ -7,8 +7,8 @@ SCRIPT_PATH=$(cd '$(dirname "$0")'; pwd)
 mkdir /usr/local/openssl
 rm -rf /usr/local/openssl/*
 rm -rf /usr/include/openssl
-rm -rf /usr/lib64/openssl/libcrypto.so
-rm -rf /usr/lib64/openssl/libssl.so
+rm -rf /usr/lib64/libcrypto.so
+rm -rf /usr/lib64/libssl.so
 
 cp -fR $SCRIPT_PATH/3rd/openssl/linux/* /usr/local/openssl/
 ln -sv /usr/local/openssl/include/openssl /usr/include/openssl
