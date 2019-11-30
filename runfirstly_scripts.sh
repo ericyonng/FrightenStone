6f7676c8ded3c234efaeb0ef996fece06174dcdc
songhuiya <120453674@qq.com>
2019-11-30 16:00:12 +0800

    【frame】linux支持openssl库

--- runfirstly_scripts.sh
+++ runfirstly_scripts.sh
@@ -6,6 +6,10 @@
 SCRIPT_PATH=$(cd '$(dirname "$0")'; pwd)
 mkdir /usr/local/openssl
 rm -rf /usr/local/openssl/*
+rm -rf /usr/include/openssl
+rm -rf /usr/lib64/openssl/libcrypto.so
+rm -rf /usr/lib64/openssl/libssl.so
+
 cp -fR $SCRIPT_PATH/3rd/openssl/linux/* /usr/local/openssl/
 ln -sv /usr/local/openssl/include/openssl /usr/include/openssl
 ln -sv /usr/local/openssl/lib/libcrypto.so.1.0.0 /usr/lib64/libcrypto.so
