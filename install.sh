d83a79a77bc080abddf1ceac056cf07c44a5e7b8
songhuiya <120453674@qq.com>
2019-12-02 11:25:22 +0800

    【frame】install

--- install.sh
+++ install.sh
@@ -4,25 +4,34 @@
 #!/usr/bin/env bash
 
 SCRIPT_PATH=$(cd '$(dirname "$0")'; pwd)
-ver = "$1"
-OUTPUT_DIR = $SCRIPT_PATH/output/gmake/$ver
 DEBUG_LIBS = {"libFrightenstone_debug.so"}
 RELEASE_LIBS = {"libFrightenstone.so"}
 
-if [$ver = "debug"]
+VER = ""
+if [ -n "$1" ]
 then
-    # 创建debug版本的so连接符号
-	for libName in $DEBUG_LIBS
-	do
-	    rm -f /usr/lib64/$libName
-	    ln -sv $OUTPUT_DIR/$libName /usr/lib64/$libName
-	done
-else
-    # 创建release版本的so连接符号
-	for libName in $RELEASE_LIBS
-	do
-	    rm -f /usr/lib64/$libName
-	    ln -sv $OUTPUT_DIR/$libName /usr/lib64/$libName
-	done
+VER = "$1"
+OUTPUT_DIR = $SCRIPT_PATH/output/gmake/$VER
+
+	if [$VER = "debug"]
+	then
+		# 创建debug版本的so连接符号
+		for libName in $DEBUG_LIBS
+		do
+			rm -f /usr/lib64/$libName
+			ln -sv $OUTPUT_DIR/$libName /usr/lib64/$libName
+		done
+	else
+		# 创建release版本的so连接符号
+		for libName in $RELEASE_LIBS
+		do
+			rm -f /usr/lib64/$libName
+			ln -sv $OUTPUT_DIR/$libName /usr/lib64/$libName
+		done
+	fi
 fi
+
+
+
+
 
