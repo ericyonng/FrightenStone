f685e5ad12fa26f8be92a225a692ea7569892dc7
songhuiya <120453674@qq.com>
2019-12-02 11:33:23 +0800

    【frame】install

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@
 
 # 变量与等号之间不可以有空格，否则会被当成命令
 SCRIPT_PATH=$(cd '$(dirname "$0")'; pwd)
-DEBUG_LIBS={"libFrightenstone_debug.so"}
-RELEASE_LIBS={"libFrightenstone.so"}
+DEBUG_LIBS={ "libFrightenstone_debug.so" }
+RELEASE_LIBS={ "libFrightenstone.so" }
 
 if [ -n "$1" ]
 then
 VER="$1"
 OUTPUT_DIR = $SCRIPT_PATH/output/gmake/$VER
 
-	if [$VER = "debug"]
+	if [ $VER = "debug" ]
 	then
 		# 创建debug版本的so连接符号
 		for libName in $DEBUG_LIBS
