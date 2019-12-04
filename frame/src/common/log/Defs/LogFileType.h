c36b0c3e2155fca847d28283d4f5d35438e41b85
songhuiya <120453674@qq.com>
2019-12-04 09:56:31 +0800

    【frame】整理log的接口暴露

---
+++ frame/src/common/log/Defs/LogFileType.h
@@ -0,0 +1,64 @@
+/*!
+ * MIT License
+ *
+ * Copyright (c) 2019 ericyonng<120453674@qq.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @file  : LogFileType.h
+ * @author: ericyonng<120453674@qq.com>
+ * @date  : 2019/11/27
+ * @brief :
+ * 
+ *
+ * 
+ */
+#ifndef __Frame_Src_Common_Log_Defs_LogFileType_H__
+#define __Frame_Src_Common_Log_Defs_LogFileType_H__
+#pragma once
+
+#include "FrightenStone/exportbase.h"
+#include "FrightenStone/common/basedefs/BaseDefs.h"
+
+FS_NAMESPACE_BEGIN
+
+// 请在LogDefs.h添加响应的日志文件定义，
+// 并在virtual Int32 FS_Log::_GetLogFileIndex(Int32 logTypeEnum); 填充响应的实现
+class BASE_EXPORT LogFileType
+{
+public:
+    enum 
+    {
+        Begin = 0,
+        Crash = Begin,
+        Details,
+        Memleak,
+        Mempool,
+        ObjPool,
+        Net,
+        Sys,
+        Any,
+        Custom,
+        Testcode,
+    };
+};
+
+FS_NAMESPACE_END
+
+#endif
