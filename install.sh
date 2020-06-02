4189d387d477f40ae246a72039688fd14df5f648
ericyonng <120453674@qq.com>
2020-06-03 00:11:29 +0800

    【frame】构建脚本

--- install.sh
+++ install.sh
@@ -9,27 +9,26 @@ DEBUG_LIBS=("libFrightenstone_debug.so")
 RELEASE_LIBS=("libFrightenstone.so")
 OPEN_COREDUMP="opencoredump"
 COREDUMPFLAG="$2"
+OUTPUT_DIR=${SCRIPT_PATH}/output/gmake/
+sudo ln -sv $SCRIPT_PATH/Service/Cfgs ${OUTPUT_DIR}/Cfgs
 
 if [ -n "$1" ]
 then
 VER="$1"
-OUTPUT_DIR=${SCRIPT_PATH}/output/gmake/
-ls ${OUTPUT_DIR}
-
 	if [ $VER = "debug" ]
 	then
 		# 创建debug版本的so连接符号
 		for libName in $DEBUG_LIBS
 		do
-			sudo rm -f /usr/lib64/$libName
-			sudo ln -sv ${OUTPUT_DIR}/$libName /usr/lib64/$libName
+			sudo rm -f /usr/lib/$libName
+			sudo ln -sv ${OUTPUT_DIR}/$libName /usr/lib/$libName
 		done
 	else
 		# 创建release版本的so连接符号
 		for libName in $RELEASE_LIBS
 		do
-			sudo rm -f /usr/lib64/$libName
-			sudo ln -sv ${OUTPUT_DIR}/$libName /usr/lib64/$libName
+			sudo rm -f /usr/lib/$libName
+			sudo ln -sv ${OUTPUT_DIR}/$libName /usr/lib/$libName
 		done
 	fi
 	
@@ -93,9 +92,9 @@ ls ${OUTPUT_DIR}
 	
 	# 生效
 	sudo sysctl -p
+else
+    echo "install with no params please check!"
 fi
-
-sudo ln -sv $SCRIPT_PATH/Service/Cfgs ${OUTPUT_DIR}/Cfgs
 
 
 
