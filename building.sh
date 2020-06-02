075635e15446cf624ca9b78c1e49e04571823242
ericyonng <120453674@qq.com>
2020-06-02 23:20:23 +0800

    【frame】构建脚本

--- building.sh
+++ building.sh
@@ -10,15 +10,20 @@ SCRIPT_PATH="$(cd $(dirname $0); pwd)"
 # OPEN_COREDUMP="opencoredump"
 # COREDUMPFLAG="$2"
 
+# 配置环境变量
+sudo export PATH=$PATH:${SCRIPT_PATH}/output/gmake/
+sudo ln -sv $SCRIPT_PATH/Service/Cfgs ${OUTPUT_DIR}/Cfgs
+
 if [ -n "$1" ]
 then
 VER="$1"
 OUTPUT_DIR=${SCRIPT_PATH}/output/gmake/
+sudo export PATH=$PATH:${SCRIPT_PATH}/output/gmake/
 
 	if [ $VER = "debug" ]
 	then
-	    sudo rm -f /usr/lib64/libFrightenstone_debug.so
-		sudo ln -sv ${OUTPUT_DIR}/libFrightenstone_debug.so /usr/lib64/libFrightenstone_debug.so
+	    sudo rm -f /usr/lib/libFrightenstone_debug.so
+		sudo ln -sv ${OUTPUT_DIR}/libFrightenstone_debug.so /usr/lib/libFrightenstone_debug.so
 		# 创建debug版本的so连接符号
 		# for libName in $DEBUG_LIBS
 		# do
@@ -26,8 +31,8 @@ OUTPUT_DIR=${SCRIPT_PATH}/output/gmake/
 		#	sudo ln -sv ${OUTPUT_DIR}/$libName /usr/lib64/$libName
 		# done
 	else
-		sudo rm -f /usr/lib64/libFrightenstone.so
-		sudo ln -sv ${OUTPUT_DIR}/libFrightenstone.so /usr/lib64/libFrightenstone.so
+		sudo rm -f /usr/lib/libFrightenstone.so
+		sudo ln -sv ${OUTPUT_DIR}/libFrightenstone.so /usr/lib/libFrightenstone.so
 		# 创建release版本的so连接符号
 		# for libName in $RELEASE_LIBS
 		# do
@@ -37,4 +42,3 @@ OUTPUT_DIR=${SCRIPT_PATH}/output/gmake/
 	fi
 fi
 
-sudo ln -sv $SCRIPT_PATH/Service/Cfgs ${OUTPUT_DIR}/Cfgs
--- runfirstly_scripts.sh
+++ runfirstly_scripts.sh
@@ -26,17 +26,17 @@ SCRIPT_PATH="$(cd $(dirname $0); pwd)"
 mkdir /usr/local/openssl
 rm -rf /usr/local/openssl/*
 rm -rf /usr/include/openssl
-rm -rf /usr/lib64/libcrypto.so
-rm -rf /usr/lib64/libssl.so
-rm -rf /usr/lib64/libcrypto.so.1.0.0
-rm -rf /usr/lib64/libssl.so.1.0.0
+rm -rf /usr/lib/libcrypto.so
+rm -rf /usr/lib/libssl.so
+rm -rf /usr/lib/libcrypto.so.1.0.0
+rm -rf /usr/lib/libssl.so.1.0.0
 
 cp -fR $SCRIPT_PATH/3rd/openssl/linux/* /usr/local/openssl/
 ln -sv /usr/local/openssl/include/openssl /usr/include/openssl
-ln -sv /usr/local/openssl/lib/libcrypto.so.1.0.0 /usr/lib64/libcrypto.so
-ln -sv /usr/local/openssl/lib/libssl.so.1.0.0 /usr/lib64/libssl.so
-ln -sv /usr/local/openssl/lib/libcrypto.so.1.0.0 /usr/lib64/libcrypto.so.1.0.0
-ln -sv /usr/local/openssl/lib/libssl.so.1.0.0 /usr/lib64/libssl.so.1.0.0
+ln -sv /usr/local/openssl/lib/libcrypto.so.1.0.0 /usr/lib/libcrypto.so
+ln -sv /usr/local/openssl/lib/libssl.so.1.0.0 /usr/lib/libssl.so
+ln -sv /usr/local/openssl/lib/libcrypto.so.1.0.0 /usr/lib/libcrypto.so.1.0.0
+ln -sv /usr/local/openssl/lib/libssl.so.1.0.0 /usr/lib/libssl.so.1.0.0
 
 
 # coredump设置永久生效
