aa26402dacff03b03d6e71a8956b0b97781cde6f
ericyonng <120453674@qq.com>
2020-06-10 23:00:17 +0800

    【service】testsuit

--- TestSuit/TestSuit/TestInsts.cpp
+++ TestSuit/TestSuit/TestInsts.cpp
@@ -90,8 +90,8 @@ void TestInsts::Run()
     // TestServer::Run();
     //TestUtf8C17toC11::Run();
     //TestMessageQueue2::Run();
-    // TestServer::Run();
-    TestNetBuffer::Run();
+    TestServer::Run();
+    // TestNetBuffer::Run();
     //TestMemAlign::Run();
     //TestEpoll::Run();
     //TestObjPool::Run();
