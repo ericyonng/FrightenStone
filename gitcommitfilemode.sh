40f6d1ad40a475b2a412b761e3f80ac0608cdb5d
ericyonng <120453674@qq.com>
2020-06-13 00:28:58 +0800

    【frame】git提交脚本执行权限

---
+++ gitcommitfilemode.sh
@@ -0,0 +1,22 @@
+# @file gitcommitfilemode.sh
+# @author EricYonng<120453674@qq.com>
+# @brief 提交shell脚本权限
+#!/usr/bin/env bash
+
+# 路径
+SCRIPT_PATH="$(cd $(dirname $0); pwd)"
+
+# 远程分支名
+REMOTE_BRANCH_NAME="master"
+if [ -n "$1" ]
+then
+    REMOTE_BRANCH_NAME="$1"
+fi
+
+# 远程分支地址
+REMOTE_NAME=$(git remote)
+
+sudo chmod 777 $SCRIPT_PATH/*.sh
+git update-index +x *.sh
+git commit -m "commit shell file mode"
+git push $REMOTE_NAME $REMOTE_BRANCH_NAME
